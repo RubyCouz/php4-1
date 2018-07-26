08234f0b154fb0ed3a5070368343ca79903531f9
rubycouzcedric <cousin.ruby.cedric@gmail.com>
2018-07-26 14:43:08 +0200

    exo1

---
+++ index.php
@@ -0,0 +1,21 @@
+<?php
+ ?>
+ <!DOCTYPE html>
+ <html lang="fr" dir="ltr">
+   <head>
+     <meta charset="utf-8" />
+     <title>Exercice 1</title>
+   </head>
+   <body>
+     <p>
+<?php
+function value()
+{
+  return true;
+}
+ echo value();
+
+ ?>
+     </p>
+   </body>
+ </html>
